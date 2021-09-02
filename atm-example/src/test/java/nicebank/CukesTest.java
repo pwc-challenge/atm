90f5ef72486e8ab7b67c0935816448d77387112b
Juan.Hernandez@coxautoinc.com <Juan.Hernandez@coxautoinc.com>
2021-09-02 10:14:55 -0500

    Initial project state

---
+++ atm-example/src/test/java/nicebank/CukesTest.java
@@ -0,0 +1,12 @@
+package nicebank;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"progress"})
+public class CukesTest {
+    
+}
