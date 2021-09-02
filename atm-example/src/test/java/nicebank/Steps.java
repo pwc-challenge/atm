7bc7cf5057d172693b3690ba5667b8397cab81b8
Juan.Hernandez@coxautoinc.com <Juan.Hernandez@coxautoinc.com>
2021-09-02 14:29:31 -0500

    added helper.

--- atm-example/src/test/java/nicebank/Steps.java
+++ atm-example/src/test/java/nicebank/Steps.java
@@ -23,18 +23,43 @@ public class Steps {
         }
     }
 
+    class Teller{
+        public void withdrawFrom(Account account, int dollars){
+
+        }
+    }
+
+    class KnowsMyAccount{
+
+        private Account myAccount;
+
+        public Account getMyAccount(){
+            if(myAccount == null){
+                myAccount = new Account();
+            }
+            return myAccount;   
+        }
+    }
+
+    KnowsMyAccount helper;
+
+    public Steps(){
+        helper = new KnowsMyAccount();
+    }
+
     @Given("^I have deposited \\$(\\d+\\.\\d+) in my account$") 
     public void i_have_deposited_$_in_my_account(@Transform(MoneyConverter.class)Money amount) throws Throwable{
-        Account myAccount = new Account();
-        myAccount.deposit(amount);
+        
+        helper.getMyAccount().deposit(amount);
 
-        Assert.assertEquals("Incorrect account balance -", amount, myAccount.getBalance());
+        Assert.assertEquals("Incorrect account balance -", 
+                                amount, helper.getMyAccount().getBalance());
     }
 
     @When("^I request \\$(\\d+)$")
     public void i_request_$(int amount) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        Teller teller = new Teller();
+        teller.withdrawFrom(helper.getMyAccount(), amount);
     }
 
      @Then("^\\$(\\d+) should be dispensed$")
